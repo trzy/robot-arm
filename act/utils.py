87ec316249f4cfabe0e535a2f2906f948926dd9d
Bart Trzynadlowski <bart.trzy@gmail.com>
2024-06-21 19:05:37 -0700

    Removed obsolete code

--- act/utils.py
+++ act/utils.py
@@ -15,7 +15,7 @@ e = IPython.embed
 ####################################################################################################
 # Dataset Loading
 #
-# Routines for detecting and loading the number of example episodes in the dataset directory.
+# Routines for detecting and loading the example episodes in the dataset directory.
 ####################################################################################################
 
 def get_episode_filepaths_and_camera_names(dataset_dir: str) -> Tuple[List[str], List[str]]:
@@ -42,96 +42,6 @@ def get_episode_filepaths_and_camera_names(dataset_dir: str) -> Tuple[List[str],
             elif camera_names != camera_names_this_file:
                 raise ValueError(f"{filepath}: camera names in this file ({camera_names_this_file}) do not match names in other files ({camera_names})")
     return filepaths, camera_names
-            
-
-            
-
-
-@dataclass
-class ExampleEpisode:
-    filepath: str
-    start_idx: int
-
-@dataclass
-class ExampleEpisodes:
-    episode_length: int
-    episodes: List[ExampleEpisode]
-    camera_names: List[str]
-
-    def num_episodes(self) -> int:
-        return len(self.episodes)
-
-def get_example_dirs(dataset_dir: str) -> List[str]:
-    return [ os.path.join(dataset_dir, dir) for dir in os.listdir(dataset_dir) if dir.startswith("example-") and os.path.isdir(os.path.join(dataset_dir, dir)) ]
-
-#
-# Given observations of length total_data_length, produces N starting indices from which
-# episode_length can safely be taken. E.g., if episode_length=135:
-#
-# total_data_length=135, indices=[0]
-# total_data_length=239, indices=[0, 104]
-# total_data_length=359, indices=[0, 135, 224]
-#
-def produce_episode_start_indices(total_data_length: int, episode_length: int):
-    indices = []
-    idx = -episode_length
-    while True:
-        # Try to advance but if the episode we woudl produce exceeds total data length, need to shift
-        # back to accommodate and produce a final, overlapping chunk
-        new_idx = min(idx + episode_length, total_data_length - episode_length)
-        if new_idx == idx:
-            break
-        idx = new_idx
-        indices.append(idx)
-    return indices
-
-def get_example_episodes(dataset_dir: str, chunk_size: int) -> ExampleEpisodes:
-    # Detect all episode files and find the minimum episode length
-    min_length = 1e9
-    dirs = get_example_dirs(dataset_dir=dataset_dir)
-    if len(dirs) == 0:
-        raise RuntimeError(f"No episode subdirectories (e.g., 'example-0', ...) found in {dataset_dir}")
-    filepaths = []
-    examples_too_short = []
-    camera_names: List[str] = []
-    for i in range(len(dirs)):
-        dir = dirs[i]
-        filepath = os.path.join(dir, "data.hdf5")
-        filepaths.append(filepath)
-        with h5py.File(name=filepath, mode="r") as fp:
-            length = len(fp["/action"])
-            min_length = min(min_length, length)
-            if length < chunk_size:
-                examples_too_short.append(dir)
-            camera_names_this_file = list(fp["/observations/images"].keys())
-            if len(camera_names) == 0:
-                camera_names = camera_names_this_file
-            elif camera_names != camera_names_this_file:
-                raise RuntimeError(f"All example episodes must have the same number of cameras and cameras must have the same names. File {filepath} has {camera_names_this_file} but expected {camera_names}")
-    assert len(camera_names) > 0
-
-    print(f"Minimum example episode length: {min_length}")
-    if len(examples_too_short) > 0:
-        raise RuntimeError(f"Example episode length must be greater than or equal to chunk_size {chunk_size}. Remove the following episodes that are too small and try again: {', '.join(examples_too_short)}")
-
-    # Chop up episodes into sub-episodes, each having min_length
-    example_episodes: List[ExampleEpisode] = []
-    i = 0
-    for filepath in filepaths:
-        with h5py.File(name=filepath, mode="r") as fp:
-            # Determine how many sub-episodes we need to create and what their start indices are
-            current_length = fp["/action"].shape[0]
-            start_indices = produce_episode_start_indices(total_data_length=current_length, episode_length=min_length)
-
-            # Create a sub-episode for each
-            for idx in start_indices:
-                example_episodes.append(ExampleEpisode(filepath=filepath, start_idx=idx))
-                print(f"Episode {i}: {filepath} at [{idx}:{idx+min_length}]")
-                i += 1
-
-    # Return the example episodes we will train and validate on
-    print(f"Found {len(example_episodes)} example episodes in {len(filepaths)} files")
-    return ExampleEpisodes(episode_length=min_length, episodes=example_episodes, camera_names=camera_names)
 
 
 ####################################################################################################
